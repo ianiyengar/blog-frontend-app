62f297ac0cc553616f7ae0253a700449fedf35c5
Ian <1984therocker@gmail.com>
2021-06-15 21:25:19 -0500

    Fix Url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sheltered-badlands-15393.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sheltered-badlands-15393.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
